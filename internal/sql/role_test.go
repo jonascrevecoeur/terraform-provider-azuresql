045c3d7a1036755ea050e0d52ddffc47c0f5ea0c
Jonas Crevecoeur <jonas.crevecoeur@hotmail.com>
2023-10-10 16:46:27 +0200

    Fix: force recreate on role in synapse when name changes

---
+++ internal/sql/role_test.go
@@ -0,0 +1,47 @@
+package sql
+
+import (
+	"terraform-provider-azuresql/internal/logging"
+	"testing"
+)
+
+func TestParseRoleId(t *testing.T) {
+	ctx := logging.GetTestContext()
+
+	tests := map[string]struct {
+		expectedConnection  string
+		expectedPrincipalId int64
+		expectError         bool
+	}{
+		"sqlserver::server:1433/role/11": {
+			expectedConnection:  "sqlserver::server:1433",
+			expectedPrincipalId: 11,
+			expectError:         false,
+		},
+	}
+
+	for roleId, expected := range tests {
+		actual := parseRoleId(ctx, roleId)
+
+		if logging.HasError(ctx) && !expected.expectError {
+			t.Errorf("Parsing roleId %s should not throw an error", roleId)
+			continue
+		}
+		if !logging.HasError(ctx) && expected.expectError {
+			t.Errorf("Parsing roleId %s should throw an error", roleId)
+			continue
+		}
+
+		if actual.Connection != expected.expectedConnection {
+			t.Errorf("Expected connectionString %s does not match actual %s when parsing roleId %s",
+				expected.expectedConnection, actual.Connection, roleId)
+		}
+
+		if actual.PrincipalId != expected.expectedPrincipalId {
+			t.Errorf("Expected principal id %d does not match actual %d when parsing roleId %s",
+				expected.expectedPrincipalId, actual.PrincipalId, roleId)
+		}
+
+		logging.ClearDiagnostics(ctx)
+	}
+}
