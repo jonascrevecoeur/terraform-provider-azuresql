5a02887292215b2d12917bb2cf2625f4256d597b
Corey Regan <regan.corey@gmail.com>
2025-06-15 23:07:38 -0700

    define SQLLoginPasswordPropertiesResourceModel

--- internal/services/sqllogin/main.go
+++ internal/services/sqllogin/main.go
@@ -11,10 +11,18 @@ type SQLLoginDataSourceModel struct {
 	Sid    types.String `tfsdk:"sid"`
 }
 
+type SQLLoginPasswordPropertiesResourceModel struct {
+	Length             types.Int32  `tfsdk:"length"`
+	AllowedSpecialChar types.String `tfsdk:"allowed_special_characters"`
+	MinSpecialChar     types.Int32  `tfsdk:"min_special_characters"`
+	MinNum             types.Int32  `tfsdk:"min_numbers"`
+	MinUpperCaseum     types.Int32  `tfsdk:"min_uppercase"`
+}
+
 type SQLLoginResourceModel struct {
 	Id       types.String `tfsdk:"id"`
 	Server   types.String `tfsdk:"server"`
 	Name     types.String `tfsdk:"name"`
-	Password types.String `tfsdk:"password"`
+	Password types.Object `tfsdk:"password"`
 	Sid      types.String `tfsdk:"sid"`
 }
